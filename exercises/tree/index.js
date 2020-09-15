43f79a086f7a8f92d1f24cefb3e1fbb6d07c09fe
Dan Seagren <seagrendaniel562@gmail.com>
2020-09-15 12:59:15 -0700

    finish tree solutions

--- exercises/tree/index.js
+++ exercises/tree/index.js
@@ -25,11 +25,30 @@ class Node {
       return node.data !== data
     })
   }
-
 }
 
 class Tree {
+  constructor() {
+    this.root = null
+  }
 
+  traverseBF(fn) {
+    const arr = [this.root]
+    while(arr.length) {
+      const node = arr.shift()
+      arr.push(...node.children)
+      fn(node)
+    }
+  }
+
+  traverseDF(fn) {
+    const arr = [this.root]
+    while(arr.length) {
+      const node = arr.shift()
+      arr.unshift(...node.children)
+      fn(node)
+    }
+  }
 }
 
 module.exports = { Tree, Node };
