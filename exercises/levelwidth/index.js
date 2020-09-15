33456ed7abda38a556597aa7675b6ffc31c3952a
Dan Seagren <seagrendaniel562@gmail.com>
2020-09-15 13:32:51 -0700

    add BST solutions

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,20 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  let widths = [0]
+  let arr = [root, 's']
+  while(arr.length > 1) {
+    const node = arr.shift()
+    if(node === 's') {
+      widths.push(0)
+      arr.push('s')
+    } else {
+      arr.push(...node.children)
+      widths[widths.length - 1]++
+    }
+  }
+  return widths
+}
 
 module.exports = levelWidth;
