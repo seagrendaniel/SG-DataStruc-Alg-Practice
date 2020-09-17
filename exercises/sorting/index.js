f8fc1c509c34175f29a4150610e4aa711ff7924f
Dan Seagren <seagrendaniel562@gmail.com>
2020-09-17 14:34:08 -0700

    add merge sort solution

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -2,13 +2,13 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
-  for(let i = 0; i < arr.length; i++) {
-    for(let j = 0; j < (arr.length - i - 1); j++) {
-      if(arr[j] > arr[j+1]) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < (arr.length - i - 1); j++) {
+      if (arr[j] > arr[j + 1]) {
         let greater
         greater = arr[j]
-        arr[j] = arr[j+1]
-        arr[j+1] = greater
+        arr[j] = arr[j + 1]
+        arr[j + 1] = greater
       }
     }
   }
@@ -16,14 +16,14 @@ function bubbleSort(arr) {
 }
 
 function selectionSort(arr) {
-  for(let i = 0; i < arr.length; i++) {
+  for (let i = 0; i < arr.length; i++) {
     let indexOfMin = i
-    for(j = i + 1; j < arr.length; j++) {
-      if(arr[j] < arr[indexOfMin]) {
+    for (j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[indexOfMin]) {
         indexOfMin = j
       }
     }
-    if(indexOfMin !== i) {
+    if (indexOfMin !== i) {
       let lesser = arr[indexOfMin]
       arr[indexOfMin] = arr[i]
       arr[i] = lesser
@@ -32,12 +32,28 @@ function selectionSort(arr) {
   return arr
 }
 
-function mergeSort(arr) {
 
+function mergeSort(arr) {
+  if(arr.length === 1) {
+    return arr
+  }
+  const center = Math.floor(arr.length / 2)
+  const left = arr.slice(0, center)
+  const right = arr.slice(center)
+
+  return merge(mergeSort(left), mergeSort(right))
 }
 
 function merge(left, right) {
-
+  let results = []
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      results.push(left.shift())
+    } else {
+      results.push(right.shift())
+    }
+  }
+  return [...results, ...left, ...right]
 }
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
